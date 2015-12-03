aeec0fb4c3070604adb1a32e78d4880fe7933b35
Matthew Carlson <matthewfcarlson@gmail.com>
2015-12-02 23:06:58 -0700

    Update lab8app.c

--- lab8app.c
+++ lab8app.c
@@ -28,6 +28,20 @@ void *CommandQueue[COMMANDQUEUESIZE];    /* space for message queue */
 YKQ *CommandQPtr;                  			 /* actual name of queue */
 
 extern int NewPieceID;
+extern int ScreenBitMap0;
+extern int ScreenBitMap1;
+extern int ScreenBitMap2;
+extern int ScreenBitMap3;
+extern int ScreenBitMap4;
+extern int ScreenBitMap5;
+extern int NewPieceType;
+extern int NewPieceOrientation;
+extern int NewPieceColumn;
+extern int TouchdownID;
+
+int BrainMoveTo();
+int BrainRotateTo();
+
 
 void STask(void)           /* tracks statistics */
 {
@@ -82,7 +96,7 @@ void PlayerTask(){
 		//wait for command queue
 		command = (int) YKQPend(CommandQPtr); /* get next msg */
 		//update block ID
-		#if DEBUG_SIMPTRIS == 1 || 1==1
+		#if DEBUG_SIMPTRIS == 1
 		printString("Sending command ");
 		printInt(command);
 		printString(" for block ");
@@ -109,18 +123,211 @@ void PlayerTask(){
 		#endif
 	}
 } 
+//Column type is 0 if flat, 1 if a corner, 2 if not
+char side1Height = 0;
+char side2Height = 0;
+char side1Type = 0;
+char side2Type = 0;
+int desiredColumn = 0;
+int desiredRotation = 0;
+int currentColumn = 0;
+int currentRotation = 0;
+
 
 //Makes all movement decisions and puts them in the queue
 void BrainTask(){
+	
 	while(1){
 		//wait for a new piece
 		YKSemPend(SimptrisPieceSemPtr);
-		#if DEBUG_SIMPTRIS == 1
-		printString("Brain is thinking\n");
+		#if DEBUG_SIMPTRIS == 1 || 1==1
+		printString("Brain is thinking about new ");
+		printInt(NewPieceType);
+		printString(" piece.  Side1:");
+		printInt(side1Type);
+		printString("@");
+		printInt(side1Height);
+		printString(" Side2:");
+		printInt(side2Type);
+		printString("@");
+		printInt(side2Height);
+		printNewLine();
+		
 		#endif
-		YKQPost(CommandQPtr,(void*)ROTATE_RIGHT);
+		
+		currentColumn   = NewPieceColumn + 1;
+		currentRotation = NewPieceOrientation;
+		
+		switch(NewPieceType){
+			case 0: //corner piece						
+				desiredColumn = 1;
+				desiredRotation = NewPieceOrientation;
+				if (side1Type == 1 && (NewPieceOrientation != 0 || side2Type == 0)){
+					//side 1 is now flat
+					side1Type = 0;
+					desiredColumn = 3;
+					desiredRotation = 2;
+					++side1Height;
+				}				
+				else if(side1Type == 2 && (NewPieceOrientation != 2 || side2Type == 0)){
+					//side 1 is now flat
+					side1Type = 0;
+					desiredColumn = 1;
+					desiredRotation = 3;
+					++side1Height;
+				}
+				else if (side2Type == 1 && (NewPieceOrientation != 0 || side1Type == 0)){
+					//side 2 is now flat
+					side2Type = 0;
+					desiredRotation = 2;
+					desiredColumn = 6;
+					++side2Height;
+				}				
+				else if(side2Type == 2 && (NewPieceOrientation != 2 || side1Type == 0)){
+					//side 2 is now flat
+					side2Type = 0;
+					desiredColumn = 4;
+					desiredRotation = 3;
+					++side2Height;
+				}
+ 			 	else if (side1Type == 0 && (side2Type != 0 || side1Height <= side2Height)){
+				 	//maybe also check where the piece is appearing and put it on the closest side
+				 	++side1Height;
+				 	if (desiredRotation == 2){
+				 		desiredRotation = 1;
+				 	}
+				 	else if (desiredRotation == 3){
+				 		desiredRotation = 0;
+				 	}
+				 	side1Type = desiredRotation + 1;
+				 	if (desiredRotation == 0){
+				 		desiredColumn = 1;				 		
+				 	}
+				 	else if (desiredRotation == 1){
+				 		desiredColumn = 3;
+				 	}
+				}
+
+ 			 	else if (side2Type == 0 && (side1Type != 0 || side1Height >= side2Height)){
+				 	//maybe also check where the piece is appearing and put it on the closest side
+				 	++side2Height;
+					if (desiredRotation == 2){
+				 		desiredRotation = 1;
+				 	}
+				 	else if (desiredRotation == 3){
+				 		desiredRotation = 0;
+				 	}
+				 	side2Type = desiredRotation + 1;
+				 	if (desiredRotation == 0){
+				 		desiredColumn = 4;
+				 	}
+				 	else if (desiredRotation == 1){
+				 		desiredColumn = 6;
+				 	}
+				}
+
+				else{
+					printString("-----------UNABLE TO PLACE PIECE!!!!!---------------\n");
+				}
+				break;
+			case 1: //straight piece
+				desiredColumn 	= 2; //left side
+				desiredRotation = 0;
+				 if (side2Type == 0 && (side1Type != 0 || side1Height >= side2Height)){
+				 	//maybe also check where the piece is appearing and put it on the closest side
+				 	++side2Height;
+					desiredColumn = 5; //right side		
+				}
+				else if (side1Type != 0 && side2Type != 0)
+					printString("-----------UNABLE TO PLACE PIECE!!!!!---------------\n");
+				else{
+					++side1Height;
+				}
+				break;
+		}
+		printString("Moving to:");
+		printInt(desiredColumn);
+		printString(" from:");
+		printInt(currentColumn);
+		printString(" and Rotated to:");
+		printInt(desiredRotation);
+		printString(" from:");
+		printInt(currentRotation);
+
+
+		while (BrainRotateTo());
+		printString("...");	
+		while (BrainMoveTo());
+		printString("Done");
+		printNewLine();
+		
 	}
 }
+
+//attempts a move returning 0 if there is no more moves to be made
+int BrainMoveTo(){
+	if (desiredColumn > currentColumn){
+		//printString("Slide Right\n");
+		YKQPost(CommandQPtr,(void*)SLIDE_RIGHT);
+		++currentColumn;
+	} 
+	else if (desiredColumn < currentColumn){
+		//printString("Slide Left\n");
+		YKQPost(CommandQPtr,(void*)SLIDE_LEFT);
+		--currentColumn;
+	} 
+
+	return currentColumn != desiredColumn;
+}
+//attemps a move returning 0 if there are more moves to be made
+int BrainRotateTo(){
+	if (currentColumn == 1 && currentRotation != desiredRotation){
+		if (currentColumn != desiredColumn){
+			BrainMoveTo();
+		}
+		else{
+			//move right once
+			printString("Moving right for proper rotation\n");
+			YKQPost(CommandQPtr,(void*)SLIDE_RIGHT);
+			currentColumn = 2;
+		}
+	}
+	else if (currentColumn == 6 && currentRotation != desiredRotation){
+		if (currentColumn != desiredColumn){
+			BrainMoveTo();
+		}
+		else{
+			//move left once
+			printString("Moving left for proper rotation\n");
+			YKQPost(CommandQPtr,(void*)SLIDE_LEFT);
+			currentColumn = 5;
+		}
+	}
+
+	if (desiredRotation == 3 && currentRotation == 0){
+		//printString("Rotate Right\n");
+		YKQPost(CommandQPtr,(void*)ROTATE_RIGHT);		
+		currentRotation = 3;
+	}
+	else if (desiredRotation == 0 && currentRotation == 3){
+		//printString("Rotate Left\n");
+		YKQPost(CommandQPtr,(void*)ROTATE_LEFT);
+		currentRotation = 0;
+	}
+	else if (desiredRotation < currentRotation){
+		//printString("Rotate RIGHT\n");
+		YKQPost(CommandQPtr,(void*)ROTATE_RIGHT);
+		++desiredRotation;
+	}
+	else if (desiredRotation > currentRotation){
+		//printString("Rotate LEFT\n");
+		YKQPost(CommandQPtr,(void*)ROTATE_LEFT);
+		--desiredRotation;
+	}
+	
+	return currentRotation != desiredRotation;
+}
+
 
 
 void main(void)
